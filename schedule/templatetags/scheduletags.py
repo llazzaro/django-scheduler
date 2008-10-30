70453cffec0c15ce5a8944ec2a5765cde064d617
Tony Hauber <thauber@gmail.com>
2008-10-30 05:17:05 +0000

    fixed create_or_edit_event view and added template tag
    git-svn-id: http://django-schedule.googlecode.com/svn/trunk@28 9e2c08a0-7048-0410-8a96-15c479260e4f

--- schedule/templatetags/scheduletags.py
+++ schedule/templatetags/scheduletags.py
@@ -4,22 +4,23 @@ from schedule.models import Calendar
 register = template.Library()
 
 class CalendarNode(template.Node):
-    def __init__(self, content_object, distinction):
+    def __init__(self, content_object, distinction, context_var):
         self.content_object = template.Variable(content_object)
         self.distinction = distinction
+        self.context_var = context_var
     
     def render(self, context):
-        context['calendar'] = Calendar.objects.get_calendar_for_object(self.content_object.resolve(context), self.distinction)
+        context[self.context_var] = Calendar.objects.get_calendar_for_object(self.content_object.resolve(context), self.distinction)
         return ''
 
 def do_get_calendar_for_object(parser, token):
-    if len(token.split_contents()) == 2:
-        tag_name, content_object = token.split_contents()
+    if len(token.split_contents()) == 4:
+        tag_name, content_object, _, context_var = token.split_contents()
         distinction = None
-    elif len(token.split_contents()) == 3:
-        tag_name, content_object, distinction = token.split_contents()
+    elif len(token.split_contents()) == 5:
+        tag_name, content_object, distinction, _, context_var = token.split_contents()
     else:
-        raise template.TemplateSyntaxError, "%r tag requires only one argument" % token.contents.split()[0]
-    return CalendarNode(content_object, distinction)
+        raise template.TemplateSyntaxError, "%r tag follows form <tag_name> <content_object> as <context_var>" % token.contents.split()[0]
+    return CalendarNode(content_object, distinction, context_var)
     
 register.tag('get_calendar_for_object', do_get_calendar_for_object)
