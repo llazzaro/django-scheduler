84aa374e2241966f3a9566b6e08bf909ccceef03
Yann Malet <yann.malet@gmail.com>
2008-12-07 00:10:28 +0000

    Fix #19 -- This is the last modification to start complete the i18n setup. There is as of now a french and german translation.
    git-svn-id: http://django-schedule.googlecode.com/svn/trunk@54 9e2c08a0-7048-0410-8a96-15c479260e4f

--- project_sample/settings.py
+++ project_sample/settings.py
@@ -61,8 +61,9 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
