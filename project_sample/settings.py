024fd89e34c078939b4f98622a9b3ee9a9ffc9d6
Rock Howard <rockmhoward@gmail.com>
2008-12-15 21:15:51 +0000

    Add site_media dir and change settings.py to match
    git-svn-id: http://django-schedule.googlecode.com/svn/trunk@61 9e2c08a0-7048-0410-8a96-15c479260e4f

--- project_sample/settings.py
+++ project_sample/settings.py
@@ -38,12 +38,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, "site_media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/site_media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
