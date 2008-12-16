405c838ba823e4adfface113bacd6807655e2dad
Yann Malet <yann.malet@gmail.com>
2008-12-16 19:24:43 +0000

    Here it is the modif. Split the tests that require the template in a dedicated app schedule_tests. As of now schedule when used as a resuable application break the test suite because it does not find information that are defined in the project_sample.
    git-svn-id: http://django-schedule.googlecode.com/svn/trunk@66 9e2c08a0-7048-0410-8a96-15c479260e4f

--- schedule/tests.py
+++ schedule/tests.py
@@ -2,7 +2,6 @@ import datetime
 import os
 
 from django.test import TestCase
-from django.test import Client
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
@@ -193,75 +192,3 @@ class TestDay(TestCase):
         period = self.day.get_time_slot( slot_start, slot_end )
         self.assertEqual( period.start, slot_start )
         self.assertEqual( period.end, slot_end )
-
-c = Client()
-
-class TestUrls(TestCase):
-    schedule_fixture = os.path.join(getattr(settings, 'PROJECT_DIR'),
-                                    "fixtures/schedule.json")
-
-    fixtures = [schedule_fixture]
-
-    def test_calendar_view(self):
-        self.response = c.get(reverse("s_calendar", kwargs={"calendar_id":1}), {})
-        self.assertEqual(self.response.status_code, 200)
-        self.assertEqual(self.response.context[0]["calendar"].name,
-                         "work")
-
-    def test_calendar_date_view(self):
-        self.response = c.get(reverse("s_calendar_date",
-                                      kwargs={
-                                        "calendar_id":1,
-                                        "year":2008,
-                                        "month":11
-                                        }),
-                              {})
-        self.assertEqual(self.response.status_code, 200)
-        self.assertEqual(self.response.context[0]["calendar"].name,
-                         "work")
-        month = self.response.context[0]["month"]
-        self.assertEqual((month.start,month.end),
-                         (datetime.datetime(2008, 11, 1, 0, 0), datetime.datetime(2008, 12, 1, 0, 0)))
-
-    def test_event_creation_anonymous_user(self):
-        self.response = c.get(reverse("s_create_event_in_calendar",
-                                      kwargs={"calendar_id":1}),
-                              {})
-        self.assertEqual(self.response.status_code, 302)
-
-    def test_event_creation_authenticated_user(self):
-        c.login(username="admin", password="admin")
-        self.response = c.get(reverse("s_create_event_in_calendar",
-                                      kwargs={"calendar_id":1}),
-                              {})
-        self.assertEqual(self.response.status_code, 200)
-        self.response = c.post(reverse("s_create_event_in_calendar",
-                                      kwargs={"calendar_id":1}),
-                               {'description': 'description',
-                                'title': 'title',
-                                'end_recurring_period_1': '10:22:00','end_recurring_period_0': '2008-10-30', 'end_recurring_period_2': 'AM',
-                                'end_1': '10:22:00','end_0': '2008-10-30', 'end_2': 'AM',
-                                'start_0': '2008-10-30','start_1': '09:21:57', 'start_2': 'AM'
-                               })
-        self.assertEqual(self.response.status_code, 302)
-        self.response = c.get(reverse("s_event",kwargs={"event_id":2}), {})
-        self.assertEqual(self.response.status_code, 200)
-        c.logout()
-
-    def test_view_event(self):
-        self.response = c.get(reverse("s_event",kwargs={"event_id":1}), {})
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_delete_event_anonymous_user(self):
-        self.response = c.get(reverse("s_delete_event",kwargs={"event_id":1}), {})
-        self.assertEqual(self.response.status_code, 302)
-
-    def test_delete_event_authenticated_user(self):
-        c.login(username="admin", password="admin")
-        self.response = c.get(reverse("s_delete_event",kwargs={"event_id":1}), {})
-        self.assertEqual(self.response.status_code, 200)
-        self.response = c.post(reverse("s_delete_event",kwargs={"event_id":1}), {})
-        self.assertEqual(self.response.status_code, 302)
-        self.response = c.get(reverse("s_delete_event",kwargs={"event_id":1}), {})
-        self.assertEqual(self.response.status_code, 404)
-        c.logout()
