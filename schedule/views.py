a5afe35c670a69821f10bf532853acf9c56c0f13
Tony Hauber <thauber@gmail.com>
2009-04-03 13:16:17 -0500

    Added documentation to the veiws.  They are still untested.  This is still an unstable release

--- schedule/views.py
+++ schedule/views.py
@@ -15,30 +15,90 @@ from schedule.periods import weekday_names
 
 def calendar_detail(request, calendar_slug, template='schedule/calendar.html',
     periods=None):
+    """
+    This view returns a calendar.  This view should be used if you are
+    interested in the meta data of a calendar, not if you want to display a
+    calendar.  It is suggested that you use calendar_by_periods if you would
+    like to display a calendar.
+    
+    Context Variables:
+    
+    ``calendar``
+        The Calendar object designated by the ``calendar_slug``.
+    """
     calendar = get_object_or_404(Calendar, slug=calendar_slug)
     return render_to_response(template, {
         "calendar": calendar,
-        "day_names": weekday_names,
     }, context_instance=RequestContext(request))
 
-def calendar_by_periods(request, calendar, periods=None):
-    calendar = get_object_or_404(Calendar, slug=calendar_slug)
+def calendar_by_periods(request, calendar_slug, periods=None):
+    """
+    This view is for getting a calendar, but also getting periods with that
+    calendar.  Which periods you get, is designated with the list periods. You
+    can designate which date you the periods to be initialized to by passing
+    a date in request.GET. See the template tag ``query_string_for_date``
     
+    Context Variables
+    
+    ``date``
+        This was the date that was generated from the query string.
+    
+    ``periods``
+        this is a dictionary that returns the periods from the list you passed
+        in.  If you passed in Month and Day, then your dictionary would look
+        like this
+        
+        {
+            'month': <schedule.periods.Month object>
+            'day':   <schedule.periods.Day object>
+        }
+        
+        So in the template to access the Day period in the context you simply
+        use ``periods.day``.
+    
+    ``calendar``
+        This is the Calendar that is designated by the ``calendar_slug``.
+    
+    ``weekday_names``
+        This is for convenience. It returns the local names of weekedays for
+        internationalization.
+        
+    """
+    calendar = get_object_or_404(Calendar, slug=calendar_slug)
     date = get_date_info(request.GET)
     if date:
         try:
-            date = datetime.datetime(get_date_info(request.GET))
+            date = datetime.datetime(**date)
         except ValueError:
             raise Http404
     else:
         date = datetime.datetime.now()
     period_objects = dict([(period.__name__, period(calendar.events.all(), date)) for period in periods])
     context = {
+        'date': date,
         'periods': period_objects,
-        'calendar': calendar
+        'calendar': calendar,
+        'weekday_names': weekday_names,
     }
 
 def event(request, event_id=None):
+    """
+    This view is for showing an event. It is important to remember that an 
+    event is not an occurrence.  Events define a set of reccurring occurrences.
+    If you would like to display an occurrence (a single instance of a 
+    recurring event) use occurrence.
+    
+    Context Variables:
+    
+    event
+        This is the event designated by the event_id
+    
+    back_url
+        this is the url that referred to this view.
+    
+    calendar
+        this is the calendar that the event is linked to.
+    """
     event = get_object_or_404(Event, id=event_id)
     back_url = request.META.get('HTTP_REFERER', None)
     try:
@@ -51,7 +111,51 @@ def event(request, event_id=None):
         "calendar" : cal,
     }, context_instance=RequestContext(request))
 
-def occurrence(request, event_id, occurrence_id=None):
+def occurrence(request, *args, **kwargs):
+    """
+    This view is used to display an occurrence.
+    
+    Context Variables:
+    
+    ``event``
+        the event that produces the occurrence
+    
+    ``occurrence`` the occurrence to be displayed
+    """
+    event, occurrence = get_occurrence(*args, **kwargs)
+    return render_to_response('schedule/event.html', {
+        'event': event
+        'occurrence': occurrence
+    }, context_instance=RequestContex(request))
+
+# Not implemented yet
+def edit_occurrence(request, *args, **kwargs):
+    event, occurrence = get_occurrence(*args, **kwargs)
+    raise Http404, "Not implemented yet"
+
+def cancel_occurrence(request, *args, **kwargs):
+    """
+    This view is used to cancel an occurrence. If it is called with a POST it
+    will cancel the view. If it is called with a GET it will ask for
+    conformation to cancel.
+    """
+    if request.GET:
+        return render_to_response(template_name)
+    event, occurrence = get_occurrence(*args, **kwargs)
+    occurrence.cancel()
+    return HttpResponseRedirect(next)
+    
+    
+
+def get_occurrence(request, event_id, occurrence_id=None, year=None, month=None,
+    day=None, hour=None, minute=None, second=None):
+    """
+    Because occurrences don't have to be persisted, there must be two ways to
+    retrieve them. both need an event, but if its persisted the occurrence can
+    be retrieved with an id. If it is not persisted it takes a date to
+    retrieve it.  This function returns an event and occurrence regardless of
+    which method is used.
+    """
     if(occurrence_id):
         occurrence = get_object_or_404(Occurrence, id=occurrence_id)
         event = occurrence.event
@@ -59,11 +163,12 @@ def occurrence(request, event_id, occurrence_id=None):
         event = get_object_or_404(Event, id=event_id)
         occurrence = event.get_occurrence(
             datetime.datetime(year, month, day, hour, minute, second))
-    return render_to_response('schedule/event.html', {
-        'event': event,
-        'occurrence': occurrence
-    }, context_instance=RequestContext(request))
-    
+        if occurrence is None:
+            raise 404
+    else:
+        raise Http404
+    return event, occurrence
+
 
 @login_required
 def create_or_edit_event(request, calendar_id=None, event_id=None, redirect=None):
@@ -71,10 +176,21 @@ def create_or_edit_event(request, calendar_id=None, event_id=None, redirect=None
     This function, if it receives a GET request or if given an invalid form in a
     POST request it will generate the following response
 
-    * Template: schedule/create_event.html
-    * Context:
-        * form: an instance of EventForm
-        * calendar: a Calendar with id=calendar_id
+    Template:
+        schedule/create_event.html
+    
+    Context Variables:
+        
+    form:
+        an instance of EventForm
+    
+    calendar: 
+        a Calendar with id=calendar_id
+
+    if this function gets a GET request with ``year``, ``month``, ``day``,
+    ``hour``, ``minute``, and ``second`` it will auto fill the form, with
+    the date specifed in the GET being the start and 30 minutes from that
+    being the end.
 
     If this form receives an event_id it will edit the event with that id, if it
     recieves a calendar_id and it is creating a new event it will add that event
@@ -87,13 +203,27 @@ def create_or_edit_event(request, calendar_id=None, event_id=None, redirect=None
     # If the key word argument redirect is set
     # Lastly redirect to the event detail of the recently create event
     """
+    date = get_date_info(request.GET)
+    inital_data = None
+    if date is not None:
+        try:
+            initial_data = {
+                "start":datetime.datetime(**date)
+                "end":datetime.timedelta(minutes=30)
+            }
+        except ValueError:
+            raise Http404
+    
     instance = None
-    if event_id:
+    if event_id is not None:
         instance = get_object_or_404(Event, id=event_id)
     calendar = None
     if calendar_id is not None:
         calendar = get_object_or_404(Calendar, id=calendar_id)
-    form = EventForm(data=request.POST or None, instance=instance, hour24=True)
+    
+    form = EventForm(data=request.POST or None, instance=instance, 
+        hour24=True, initial=initial_data)
+    
     if form.is_valid():
         event = form.save(commit=False)
         if instance is None:
@@ -105,39 +235,12 @@ def create_or_edit_event(request, calendar_id=None, event_id=None, redirect=None
         if 'next' in request.GET:
             next = check_next_url(request.GET['next']) or next
         return HttpResponseRedirect(next)
+    
     return render_to_response('schedule/create_event.html', {
         "form": form,
         "calendar": calendar
     }, context_instance=RequestContext(request))
 
-@login_required
-def create_event(request, calendar_id=None, calendar_slug=None, year=None, month=None, day=None, hour=None, minute=None, redirect=None):
-    if calendar_id:
-        calendar = get_object_or_404(Calendar, id=calendar_id)
-    elif calendar_slug:
-        calendar = get_object_or_404(Calendar, slug=calendar_slug)
-    starttime = datetime.datetime(year=int(year),month=int(month),day=int(day),hour=int(hour),minute=int(minute))
-    endtime = starttime + datetime.timedelta(minutes=30)
-    end_recur = endtime + datetime.timedelta(days=8)
-    init_values = {
-        'start' : starttime,
-        'end' : endtime,
-        'end_recurring_period' : end_recur
-    }
-    form = EventForm(data=request.POST or None, initial=init_values)
-    if form.is_valid():
-        event = form.save(commit=False)
-        event.creator = request.user
-        event.save()
-        calendar.events.add(event)
-        next = redirect or reverse('s_event', args=[event.id])
-        if 'next' in request.GET:
-            next = check_next_url(request.GET['next']) or next
-        return HttpResponseRedirect(next)
-    return render_to_response('schedule/create_event.html', {
-        "form": form,
-        "calendar": calendar
-    }, context_instance=RequestContext(request))
 
 def delete_event(request, event_id=None, redirect=None, login_required=True):
     """
