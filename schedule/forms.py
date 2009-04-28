a85f960d29df90b7ec0713b6dcc4c4b645a2482a
Bartek Górny <bartek@gorny.edu.pl>
2009-04-28 23:16:13 +0800

    form checks that end_date > sstart_date
    Signed-off-by: thauber <thauber@gmail.com>

--- schedule/forms.py
+++ schedule/forms.py
@@ -5,12 +5,21 @@ import datetime
 import time
 
 
-class EventForm(forms.ModelForm):
+class SpanForm(forms.ModelForm):
+
+    start = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
+    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget, help_text = _("The end time must be later than start time."))
+
+    def clean_end(self):
+        if self.cleaned_data['end'] <= self.cleaned_data['start']:
+            raise forms.ValidationError(_("The end time must be later than start time."))
+        return self.cleaned_data['end']
+
+
+class EventForm(SpanForm):
     def __init__(self, hour24=False, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
     
-    start = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
-    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget, help_text = _("The end time must be later than start time."))
     end_recurring_period = forms.DateTimeField(help_text = _("This date is ignored for one time only events."), required=False)
     
     class Meta:
@@ -18,10 +27,7 @@ class EventForm(forms.ModelForm):
         exclude = ('creator', 'created_on', 'calendar')
         
 
-class OccurrenceForm(forms.ModelForm):
-
-    start = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
-    end = forms.DateTimeField(widget=forms.SplitDateTimeWidget, help_text = _("The end time must be later than start time."))
+class OccurrenceForm(SpanForm):
     
     class Meta:
         model = Occurrence
