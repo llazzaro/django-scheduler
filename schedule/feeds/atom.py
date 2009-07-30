3fa571dba3412d191cd7b83fb66a176d0d90c22b
Skylar Saveland <skylar.saveland@gmail.com>
2009-07-30 18:32:00 +0800

    made the project_sample media work, log-in to add
    Signed-off-by: Bartek Górny <bartek@gorny.edu.pl>

--- schedule/feeds/atom.py
+++ schedule/feeds/atom.py
@@ -1,24 +1,24 @@
-# 
+#
 # django-atompub by James Tauber <http://jtauber.com/>
 # http://code.google.com/p/django-atompub/
 # An implementation of the Atom format and protocol for Django
-# 
+#
 # For instructions on how to use this module to generate Atom feeds,
 # see http://code.google.com/p/django-atompub/wiki/UserGuide
-# 
-# 
+#
+#
 # Copyright (c) 2007, James Tauber
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,7 +26,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# 
+#
 
 from xml.sax.saxutils import XMLGenerator
 from datetime import datetime
