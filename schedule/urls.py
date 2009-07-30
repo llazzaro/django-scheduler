8932ed5771b0f750f7d34d38354367008dac1cbe
Skylar Saveland <skylar.saveland@gmail.com>
2009-07-30 19:04:13 +0800

    jquery.tooltip is now present
    Signed-off-by: Bartek Górny <bartek@gorny.edu.pl>

--- schedule/urls.py
+++ schedule/urls.py
@@ -10,37 +10,45 @@ info_dict = {
 }
 
 urlpatterns = patterns('',
+
 # urls for Calendars
 url(r'^calendar/$',
     object_list,
     name="schedule",
     kwargs={'queryset':Calendar.objects.all(), 'template_name':'schedule/calendar_list.html'}),
+
 url(r'^calendar/year/(?P<calendar_slug>[-\w]+)/$',
-    'schedule.views.calendar_by_periods', 
+    'schedule.views.calendar_by_periods',
     name="year_calendar",
     kwargs={'periods': [Year], 'template_name': 'schedule/calendar_year.html'}),
-url(r'^calendar/tri_month/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar_by_periods', 
+
+url(r'^calendar/tri_month/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar_by_periods',
     name="tri_month_calendar",
     kwargs={'periods': [Month], 'template_name': 'schedule/calendar_tri_month.html'}),
-url(r'^calendar/compact_month/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar_by_periods', 
+
+url(r'^calendar/compact_month/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar_by_periods',
     name = "compact_calendar",
     kwargs={'periods': [Month], 'template_name': 'schedule/calendar_compact_month.html'}),
-url(r'^calendar/month/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar_by_periods', 
+
+url(r'^calendar/month/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar_by_periods',
     name = "month_calendar",
     kwargs={'periods': [Month], 'template_name': 'schedule/calendar_month.html'}),
-url(r'^calendar/week/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar_by_periods', 
+
+url(r'^calendar/week/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar_by_periods',
     name = "week_calendar",
     kwargs={'periods': [Week], 'template_name': 'schedule/calendar_week.html'}),
-url(r'^calendar/daily/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar_by_periods', 
+
+url(r'^calendar/daily/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar_by_periods',
     name = "day_calendar",
     kwargs={'periods': [Day], 'template_name': 'schedule/calendar_day.html'}),
-url(r'^calendar/(?P<calendar_slug>[-\w]+)/$', 
-    'schedule.views.calendar', 
+
+url(r'^calendar/(?P<calendar_slug>[-\w]+)/$',
+    'schedule.views.calendar',\
     name = "calendar"),
 
 #Event Urls
