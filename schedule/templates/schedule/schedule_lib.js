c471d4edc84fc427effe2e9a98d7608ab432ba7a
Bartek Górny <bartek@gorny.edu.pl>
2009-10-20 22:28:15 +0200

    code formatting and cleanup

--- schedule/templates/schedule/schedule_lib.js
+++ schedule/templates/schedule/schedule_lib.js
@@ -1,7 +1,8 @@
 
-edit_occurrence_url = "{% url edit_occurrence_by_code %}"
+
 
 function lead_zero(v){
+    /* utility function for formatting datetime */
     if(v<10){
         v = '0' + v;
     }
@@ -9,9 +10,15 @@ function lead_zero(v){
 }
 
 function format_datetime(dt){
+    /* takes a Date object and returns formatted string which is accepted
+     * by Django form
+     **/
     var s = ''
     s = s + dt.getFullYear() + '-' + lead_zero(dt.getMonth()+1) + '-' + lead_zero(dt.getDate());
     s = s + ' '
     s = s + lead_zero(dt.getHours()) + ':' + lead_zero(dt.getMinutes()) + ':' + lead_zero(dt.getSeconds());
     return s
 }
+
+/* global var for use by ajax scripts */
+edit_occurrence_url = "{% url edit_occurrence_by_code %}"
--- schedule/templates/schedule/weekcalendar.js
+++ schedule/templates/schedule/weekcalendar.js
@@ -28,53 +28,51 @@ $(document).ready(function() {
     var $calendar = $('#calendar');
     var id = 10;
 
-	$calendar.weekCalendar({
-    /* TODO input parameters dynamically from settings */
-		timeslotsPerHour : 4,
-		allowCalEventOverlap : true,
+    $calendar.weekCalendar({
+         /* TODO input parameters dynamically from settings */
+        timeslotsPerHour : 4,
+        allowCalEventOverlap : true,
         firstDayOfWeek : 1,
         businessHours :{start: 8, end: 18, limitDisplay: true },
-		height : function($calendar) {
-			return $(window).height() - $("h1").outerHeight();
-		},
-		eventRender : function(calEvent, $event) {
-			if (calEvent.end.getTime() < new Date().getTime()) {
-        /* past events grayed out */
-        /* TODO this should be (a) optional, (b) styled by CSS file */
-				$event.css("backgroundColor", "#aaa");
-				$event.find(".time").css({
-							"backgroundColor" : "#999",
-							"border" : "1px solid #888"
-						});
-			}
-		},
+        height : function($calendar) {
+            return $(window).height() - $("h1").outerHeight();
+        },
+        eventRender : function(calEvent, $event) {
+            if (calEvent.end.getTime() < new Date().getTime()) {
+                /* past events grayed out */
+                /* TODO this should be (a) optional, (b) styled by CSS file */
+                $event.css("backgroundColor", "#aaa");
+                $event.find(".time").css({
+                            "backgroundColor" : "#999",
+                            "border" : "1px solid #888"
+                        });
+            }
+        },
         draggable : function(calEvent, $event) {
-           return calEvent.readOnly != true;
+            return calEvent.readOnly != true;
         },
         resizable : function(calEvent, $event) {
             return calEvent.readOnly != true;
         },
-		eventNew : function(calEvent, $event) {
+        eventNew : function(calEvent, $event) {
             var $dialogContent = $("#event_edit_container");
             resetForm($dialogContent);
             var startField = $dialogContent.find("select[name='start']").val(calEvent.start);
             var endField =  $dialogContent.find("select[name='end']").val(calEvent.end);
             var titleField = $dialogContent.find("input[name='title']");
             var bodyField = $dialogContent.find("textarea[name='body']");
-
-
             $dialogContent.dialog({
                 modal: true,
                 title: "New Calendar Event",
                 close: function() {
-                   $dialogContent.dialog("destroy");
-                   $dialogContent.hide();
-                   $('#calendar').weekCalendar("removeUnsavedEvents");
+                    $dialogContent.dialog("destroy");
+                    $dialogContent.hide();
+                    $('#calendar').weekCalendar("removeUnsavedEvents");
                 },
                 buttons: {
                     save : function(){
                         /* TODO save old params, send AJAX request to save new data, restore old data upon receiving
-                         * error message from server */
+                        * error message from server */
                         calEvent.id = id;
                         id++;
                         calEvent.start = new Date(startField.val());
@@ -91,23 +89,20 @@ $(document).ready(function() {
                     }
                 }
             }).show();
-
             $dialogContent.find(".date_holder").text($calendar.weekCalendar("formatDate", calEvent.start));
             setupStartAndEndTimeFields(startField, endField, calEvent, $calendar.weekCalendar("getTimeslotTimes", calEvent.start));
             $(window).resize().resize(); //fixes a bug in modal overlay size ??
+        },
 
+        eventDrop : function(calEvent, $event) {
+            weekcal_save_event(calEvent);
+        },
 
-		},
-		eventDrop : function(calEvent, $event) {
-                        weekcal_save_event(calEvent);
+        eventResize : function(calEvent, $event) {
+            weekcal_save_event(calEvent);
+        },
 
-                },
-
-		eventResize : function(calEvent, $event) {
-                        weekcal_save_event(calEvent);
-		},
-		eventClick : function(calEvent, $event) {
-
+        eventClick : function(calEvent, $event) {
             if(calEvent.readOnly) {
                 return;
             }
@@ -125,33 +120,33 @@ $(document).ready(function() {
                 modal: true,
                 title: "Edit - " + calEvent.title,
                 close: function() {
-                   $dialogContent.dialog("destroy");
-                   $dialogContent.hide();
-                   $('#calendar').weekCalendar("removeUnsavedEvents");
+                    $dialogContent.dialog("destroy");
+                    $dialogContent.hide();
+                    $('#calendar').weekCalendar("removeUnsavedEvents");
                 },
                 buttons: {
-                    save : function(){
-                        /* send new data to the server; if response is OK
-                         * then update calendar and close dialog */
-                        start = new Date(startField.val());
-                        end = new Date(endField.val());
-                        title = titleField.val();
-                        body = bodyField.val();
-                        st = format_datetime(start);
-                        en = format_datetime(end);
-                        data = {id:calEvent.id, start:st, end:en, title:title, description:body};
-                        $.post(edit_occurrence_url, data, function(data){
-                            if(data=='OK'){
-                                calEvent.start = start;
-                                calEvent.end = end;
-                                calEvent.title = title;
-                                calEvent.body = body;
-                                $dialogContent.dialog("close");
-                                $calendar.weekCalendar("updateEvent", calEvent);
-                            }else{
-                                alert(data);
-                            }
-                        });
+                save : function(){
+                    /* send new data to the server; if response is OK
+                    * then update calendar and close dialog */
+                    start = new Date(startField.val());
+                    end = new Date(endField.val());
+                    title = titleField.val();
+                    body = bodyField.val();
+                    st = format_datetime(start);
+                    en = format_datetime(end);
+                    data = {id:calEvent.id, start:st, end:en, title:title, description:body};
+                    $.post(edit_occurrence_url, data, function(data){
+                        if(data=='OK'){
+                            calEvent.start = start;
+                            calEvent.end = end;
+                            calEvent.title = title;
+                            calEvent.body = body;
+                            $dialogContent.dialog("close");
+                            $calendar.weekCalendar("updateEvent", calEvent);
+                        }else{
+                            alert(data);
+                        }
+                    });
 
                     },
                     "delete" : function(){
@@ -168,31 +163,34 @@ $(document).ready(function() {
             endField =  $dialogContent.find("select[name='end']").val(calEvent.end);
             $dialogContent.find(".date_holder").text($calendar.weekCalendar("formatDate", calEvent.start));
             setupStartAndEndTimeFields(startField, endField, calEvent, $calendar.weekCalendar("getTimeslotTimes", calEvent.start));
-		    $(window).resize().resize(); //fixes a bug in modal overlay size ??
+            $(window).resize().resize(); //fixes a bug in modal overlay size ??
 
         },
-		eventMouseover : function(calEvent, $event) {
-		},
-		eventMouseout : function(calEvent, $event) {
-		},
-		noEvents : function() {
 
-		},
-		data : function(start, end, callback) {
-                /* this is called (a) upon page load and (b) when week is changed
-                 * start and end are beginning/end of selected week */
-                var base_url = '{% url week_calendar_json calendar_slug=calendar_slug %}';
-                var url = base_url + '?year=' + start.getFullYear() + '&month=' + (start.getMonth() + 1) + '&day=' + start.getDate();
-                $.getJSON(url, function(data){
-                    res = {events:data};
-                    callback(res);
-                });
-            }
-	});
+        eventMouseover : function(calEvent, $event) {
+        },
+
+        eventMouseout : function(calEvent, $event) {
+        },
+
+        noEvents : function() {
+        },
+
+        data : function(start, end, callback) {
+            /* this is called (a) upon page load and (b) when week is changed
+            * start and end are beginning/end of selected week */
+            var base_url = '{% url week_calendar_json calendar_slug=calendar_slug %}';
+            var url = base_url + '?year=' + start.getFullYear() + '&month=' + (start.getMonth() + 1) + '&day=' + start.getDate();
+            $.getJSON(url, function(data){
+                res = {events:data};
+                callback(res);
+            });
+        }
+    });
 
     function resetForm($dialogContent) {
-         $dialogContent.find("input").val("");
-         $dialogContent.find("textarea").val("");
+        $dialogContent.find("input").val("");
+        $dialogContent.find("textarea").val("");
     }
 
     function getTestEventData() {
@@ -202,12 +200,12 @@ $(document).ready(function() {
 
         return {
             events : [
-               {"id":1, "start": new Date(year, month, day, 12), "end": new Date(year, month, day, 13, 30),"title":"Lunch with Mike"},
-               {"id":2, "start": new Date(year, month, day, 14), "end": new Date(year, month, day, 14, 45),"title":"Dev Meeting"},
-               {"id":3, "start": new Date(year, month, day + 1, 17), "end": new Date(year, month, day + 1, 17, 45),"title":"Hair cut"},
-               {"id":4, "start": new Date(year, month, day - 1, 8), "end": new Date(year, month, day - 1, 9, 30),"title":"Team breakfast"},
-               {"id":5, "start": new Date(year, month, day + 1, 14), "end": new Date(year, month, day + 1, 15),"title":"Product showcase"},
-               {"id":6, "start": new Date(year, month, day, 10), "end": new Date(year, month, day, 11),"title":"I'm read-only", readOnly : true}
+            {"id":1, "start": new Date(year, month, day, 12), "end": new Date(year, month, day, 13, 30),"title":"Lunch with Mike"},
+            {"id":2, "start": new Date(year, month, day, 14), "end": new Date(year, month, day, 14, 45),"title":"Dev Meeting"},
+            {"id":3, "start": new Date(year, month, day + 1, 17), "end": new Date(year, month, day + 1, 17, 45),"title":"Hair cut"},
+            {"id":4, "start": new Date(year, month, day - 1, 8), "end": new Date(year, month, day - 1, 9, 30),"title":"Team breakfast"},
+            {"id":5, "start": new Date(year, month, day + 1, 14), "end": new Date(year, month, day + 1, 15),"title":"Product showcase"},
+            {"id":6, "start": new Date(year, month, day, 10), "end": new Date(year, month, day, 11),"title":"I'm read-only", readOnly : true}
 
             ]
         };
@@ -215,12 +213,12 @@ $(document).ready(function() {
 
 
     /*
-     * Sets up the start and end time fields in the calendar event
-     * form for editing based on the calendar event being edited
-     */
+    * Sets up the start and end time fields in the calendar event
+    * form for editing based on the calendar event being edited
+    */
     function setupStartAndEndTimeFields($startTimeField, $endTimeField, calEvent, timeslotTimes) {
 
-       for(var i=0; i<timeslotTimes.length; i++) {
+        for(var i=0; i<timeslotTimes.length; i++) {
             var startTime = timeslotTimes[i].start;
             var endTime = timeslotTimes[i].end;
             var startSelected = "";
@@ -233,17 +231,16 @@ $(document).ready(function() {
             }
             $startTimeField.append("<option value=\"" + startTime + "\" " + startSelected + ">" + timeslotTimes[i].startFormatted + "</option>");
             $endTimeField.append("<option value=\"" + endTime + "\" " + endSelected + ">" + timeslotTimes[i].endFormatted + "</option>");
-
         }
         $endTimeOptions = $endTimeField.find("option");
         $startTimeField.trigger("change");
     }
 
-   var $endTimeField = $("select[name='end']");
-   var $endTimeOptions = $endTimeField.find("option");
+    var $endTimeField = $("select[name='end']");
+    var $endTimeOptions = $endTimeField.find("option");
 
-   //reduces the end time options to be only after the start time options.
-   $("select[name='start']").change(function(){
+    //reduces the end time options to be only after the start time options.
+    $("select[name='start']").change(function(){
         var startTime = $(this).find(":selected").val();
         var currentEndTime = $endTimeField.find("option:selected").val();
         $endTimeField.html(
@@ -262,8 +259,8 @@ $(document).ready(function() {
         });
 
         if(!endTimeSelected) {
-           //automatically select an end date 2 slots away.
-           $endTimeField.find("option:eq(1)").attr("selected", "selected");
+            //automatically select an end date 2 slots away.
+            $endTimeField.find("option:eq(1)").attr("selected", "selected");
         }
 
     });
@@ -271,21 +268,7 @@ $(document).ready(function() {
 
     var $about = $("#about");
 
-    $("#about_button").click(function(){
-        $about.dialog({
-                title: "About this calendar demo",
-                width: 600,
-                close: function() {
-                   $about.dialog("destroy");
-                   $about.hide();
-                },
-                buttons: {
-                    close : function(){
-                        $about.dialog("close");
-                    }
-                }
-            }).show();
-    });
+
     {% if start_date %}
     /* jump to initial date */
     var year = {{start_date.year}};
@@ -294,5 +277,4 @@ $(document).ready(function() {
     var initial_date = new Date(year, month, day);
     $calendar.weekCalendar("gotoWeek", initial_date);
     {% endif %}
-
 });
