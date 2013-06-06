fec92317db425c435f678379b3a61abf3a223767
Shannon Collins <smcoll@gmail.com>
2013-06-06 11:57:50 -0500

    added MEDIA_URL value

--- project_sample/settings.py
+++ project_sample/settings.py
@@ -1,4 +1,3 @@
-# Django settings for paquetin project.
 import os
 
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
@@ -53,13 +52,13 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = PROJECT_PATH + '/media/'
+MEDIA_ROOT = PROJECT_PATH + '/site_media/'
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
