5586690b4ff1d4d482fcfaff36ce8ab137a8ed7a
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2013-09-27 01:01:25 -0300

    Rule 100% coverage

---
+++ schedule/tests/test_rule.py
@@ -0,0 +1,16 @@
+import datetime
+import pytz
+
+from django.test import TestCase
+
+from schedule.models import Rule
+
+class TestPeriod(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_params(self):
+        rule = Rule(params = "count:1;bysecond:1;byminute:1,2,4,5")
+        expected =  {'count': 1, 'byminute': [1, 2, 4, 5], 'bysecond': 1}
+        self.assertEquals(rule.get_params(), expected)
