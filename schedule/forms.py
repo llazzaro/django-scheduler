2dca6fd8d48a8588a99ebc420456172f4d2a5870
Sergey Fursov <GeyseR85@gmail.com>
2014-04-06 00:17:28 +0400

    SpanForm validations improvents, removed unused hour24 argument
    also some cleanup and formatting

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,37 +1,36 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from schedule.models import Event, Occurrence
-import datetime
-import time
 
 
 class SpanForm(forms.ModelForm):
-
     start = forms.DateTimeField(label=_("start"),
                                 widget=forms.SplitDateTimeWidget)
     end = forms.DateTimeField(label=_("end"),
-                              widget=forms.SplitDateTimeWidget, help_text = _("The end time must be later than start time."))
+                              widget=forms.SplitDateTimeWidget,
+                              help_text=_(u"The end time must be later than start time."))
 
-    def clean_end(self):
-        if self.cleaned_data['end'] <= self.cleaned_data['start']:
-            raise forms.ValidationError(_("The end time must be later than start time."))
-        return self.cleaned_data['end']
+    def clean(self):
+        if 'end' in self.cleaned_data and 'start' in self.cleaned_data:
+            if self.cleaned_data['end'] <= self.cleaned_data['start']:
+                raise forms.ValidationError(_(u"The end time must be later than start time."))
+        return self.cleaned_data
 
 
 class EventForm(SpanForm):
-    def __init__(self, hour24=False, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
-    
-    end_recurring_period = forms.DateTimeField(label=_("End recurring period"),
-                                               help_text = _("This date is ignored for one time only events."), required=False)
-    
+
+    end_recurring_period = forms.DateTimeField(label=_(u"End recurring period"),
+                                               help_text=_(u"This date is ignored for one time only events."),
+                                               required=False)
+
     class Meta:
         model = Event
         exclude = ('creator', 'created_on', 'calendar')
-        
+
 
 class OccurrenceForm(SpanForm):
-    
     class Meta:
         model = Occurrence
         exclude = ('original_start', 'original_end', 'event', 'cancelled')
