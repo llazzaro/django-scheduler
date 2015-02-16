c78ab0aaf6c0d83689b66ee3694fb9b0ba0c8ffa
Simon Westphahl <westphahl@gmail.com>
2015-02-16 17:29:58 +0100

    Fix setup.py test

--- runtests.py
+++ runtests.py
@@ -6,10 +6,14 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
+
+def runtests():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
+
+if __name__ == "__main__":
+    runtests()
