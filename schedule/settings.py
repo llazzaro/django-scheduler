8fe6898a35eb346eddda09b96493d29cda2a5a9c
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2015-07-11 02:45:38 -0300

    add some required static files

--- schedule/settings.py
+++ schedule/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''  # os.path.join(PROJECT_PATH, 'assets/')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -70,7 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'assets/'),
+    os.path.join(PROJECT_PATH, 'static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
