3ed432d8b58475f84a2c1780b14665734e2be03b
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2016-05-04 11:59:26 -0300

    increment version to 0.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-scheduler',
-    version='0.7.6',
+    version='0.8',
     description='A calendaring app for Django.',
     author='Leonardo Lazzaro',
     author_email='lazzaroleonardo@gmail.com',
