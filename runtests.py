967a606029fbbc7de70051ac66fcfa3b6c3a2d6e
Jon Dufresne <jon.dufresne@gmail.com>
2017-07-20 17:50:11 -0700

    Rename tests/test_settings.py to tests/test_settings.py
    * More conventional with other Django project (e.g.
      django-debug-toolbar)
    * Slightly shorter and easier to type
    * Doesn't use the same format as a test file (test_*.py)

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ from django.test.utils import get_runner
 
 
 def runtests():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
