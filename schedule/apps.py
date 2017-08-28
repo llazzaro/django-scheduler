52efdc05a6b650ac1e90253f6f709508d44c1d31
Jean Guzman <jeanalberick@gmil.com>
2017-08-28 13:14:37 -0600

    - added app translations - updated translation files with spanish translations

---
+++ schedule/apps.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class ScheduleConfig(AppConfig):
+    name = 'schedule'
+    
+    verbose_name = _('Schedules')
