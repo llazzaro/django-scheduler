e0aecec5f2a8a9230a295045977106e441b2495e
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-08 19:36:51 -0800

    Add merge migration to resolve multiple leaf nodes

---
+++ schedule/migrations/0009_merge.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('schedule', '0008_gfk_index'),
+        ('schedule', '0008_calendar_slug_unique'),
+    ]
