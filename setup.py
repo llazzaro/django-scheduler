4d1bb03730e5a35ea5ff7d83ab7a8571a7c56802
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2018-01-09 00:25:04 -0300

    Update new scheduler version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-scheduler',
-    version='0.8.6',
+    version='0.8.7',
     description='A calendaring app for Django.',
     author='Leonardo Lazzaro',
     author_email='lazzaroleonardo@gmail.com',
