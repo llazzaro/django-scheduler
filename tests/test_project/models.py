8377a4ffb2d2c95fa77702cf616096c4ca597f45
m-vdb <mvergerdelbove@work4labs.com>
2018-01-10 08:52:54 +0100

    use a test_project module to test the behaviour

---
+++ tests/test_project/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from schedule.models import AbstractCalendar, AbstractEvent, AbstractOccurrence
+
+
+class CustomCalendar(AbstractCalendar):
+    color = models.CharField(max_length=50)
+
+
+class CustomEvent(AbstractEvent):
+    last_read_at = models.DateTimeField(null=True)
+
+
+class CustomOccurrence(AbstractOccurrence):
+    is_ready = models.BooleanField(default=False)
---
+++ tests/test_project/settings.py
@@ -0,0 +1,84 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = '_nc9_+ntllx!ptq5tzh96nn^$a#81#auhrdw#8t#+n0#4ig(lz'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ]
+        }
+    }
+]
+
+MIDDLEWARE = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'schedule.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'schedule',
+    'tests.test_project',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Internationalization
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+]
+
+# URL prefix for static files.
+STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+# custom Scheduler models
+SCHEDULER_CALENDAR_MODEL = 'test_project.CustomCalendar'
+SCHEDULER_EVENT_MODEL = 'test_project.CustomEvent'
+SCHEDULER_OCCURRENCE_MODEL = 'test_project.CustomOccurrence'
