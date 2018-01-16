8c45306aec5d2b8b1592f7c327cecfc6bb2558a7
m-vdb <mvergerdelbove@work4labs.com>
2018-01-16 18:10:56 +0100

    __file__ is only absolute in python3

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,7 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
