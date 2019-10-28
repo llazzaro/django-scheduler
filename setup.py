053dbd49c1f79bfac4aa3f58e524d37442767605
Leonardo Lazzaro <leonardol@infobytesec.com>
2019-10-28 01:07:04 -0300

    increse version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="django-scheduler",
-    version="0.9.1",
+    version="0.9.2",
     description="A calendaring app for Django.",
     long_description=long_description,
     long_description_content_type="text/markdown",
