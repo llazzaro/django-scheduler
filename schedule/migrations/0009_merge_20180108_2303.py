2fd9755605e1450f9c5e59954fdb9c87a51d1f5c
Leonardo Lazzaro <leonardol@infobytesec.com>
2019-10-28 01:06:42 -0300

    fix borken migration

--- schedule/migrations/0009_merge_20180108_2303.py
+++ schedule/migrations/0009_merge_20180108_2303.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("schedule", "0008_calendar_slug_unique"),
         ("schedule", "0008_gfk_index"),
     ]
 
