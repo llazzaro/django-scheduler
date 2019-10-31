a18189ef806a2917154ebaf990a955c9d5a8a16c
Jon Dufresne <jon.dufresne@gmail.com>
2019-10-31 15:07:53 -0700

    Add testing for Python 3.8
    Supported by Django 3.0.
    
    Python 3.8 was released on October 14th, 2019.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Utilities",
     ],
     python_requires=">=3.5",
