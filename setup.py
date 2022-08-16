336b7b019448e11764d6a84639f79ddfe5c13448
François Freitag <mail@franek.fr>
2022-08-16 14:27:21 +0200

    Add support for Django 4.1

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
