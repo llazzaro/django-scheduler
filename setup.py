7aa94bb80ddbbc51e5f86f67e29e780bdea9043f
François Freitag <mail@franek.fr>
2022-08-16 14:33:46 +0200

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Utilities",
     ],
     python_requires=">=3.6",
