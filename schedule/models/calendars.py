d691550163941db6dbcec2f347fee4d3941615a0
Robin <robin@whoisearth.com>
2025-01-01 10:31:44 -0500

    Update calendars.py
    Django 5.1 ... index_together deprecated.

--- schedule/models/calendars.py
+++ schedule/models/calendars.py
@@ -231,7 +231,7 @@ class CalendarRelation(models.Model):
     class Meta:
         verbose_name = _("calendar relation")
         verbose_name_plural = _("calendar relations")
-        index_together = [("content_type", "object_id")]
+        indexes = [models.Index(fields=["content_type", "object_id"])]
 
     def __str__(self):
         return "{} - {}".format(self.calendar, self.content_object)
